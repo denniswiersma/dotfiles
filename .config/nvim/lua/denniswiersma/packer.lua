c7676cf9a38c2292028fa2ebd1d9b90b7d068318
denniswiersma <dennis.wiersma@icloud.com>
2023-09-19 09:05:54 +0200

    added todo-comments

--- .config/nvim/lua/denniswiersma/packer.lua
+++ .config/nvim/lua/denniswiersma/packer.lua
@@ -1,100 +1,103 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.2',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.2",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-    use({
-        "catppuccin/nvim",
-        as = "catppuccin",
-        config = function()
-            vim.cmd('colorscheme catppuccin-mocha')
-        end
-    })
+	use({
+		"catppuccin/nvim",
+		as = "catppuccin",
+		config = function()
+			vim.cmd("colorscheme catppuccin-mocha")
+		end,
+	})
 
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
-    use('theprimeagen/harpoon')
+	use("theprimeagen/harpoon")
 
-    use('mbbill/undotree')
+	use("mbbill/undotree")
 
-    use('tpope/vim-fugitive')
+	use("tpope/vim-fugitive")
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
-            {                            -- Optional
-                'williamboman/mason.nvim',
-                run = function()
-                    pcall(vim.cmd, 'MasonUpdate')
-                end,
-            },
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{ -- Optional
+				"williamboman/mason.nvim",
+				run = function()
+					pcall(vim.cmd, "MasonUpdate")
+				end,
+			},
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },     -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'L3MON4D3/LuaSnip' },     -- Required
-        }
-    }
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "L3MON4D3/LuaSnip" }, -- Required
+		},
+	})
 
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    }
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "nvim-tree/nvim-web-devicons", opt = true },
+	})
 
-    use 'm4xshen/autoclose.nvim'
+	use("m4xshen/autoclose.nvim")
 
-    use 'ggandor/leap.nvim'
+	use("ggandor/leap.nvim")
 
-    use({
-        "utilyre/barbecue.nvim",
-        tag = "*",
-        requires = {
-            "SmiteshP/nvim-navic",
-            "nvim-tree/nvim-web-devicons", -- optional dependency
-        },
-    })
+	use({
+		"utilyre/barbecue.nvim",
+		tag = "*",
+		requires = {
+			"SmiteshP/nvim-navic",
+			"nvim-tree/nvim-web-devicons", -- optional dependency
+		},
+	})
 
-    use "smjonas/inc-rename.nvim"
+	use("smjonas/inc-rename.nvim")
 
-    use {
-        'goolord/alpha-nvim',
-        requires = { 'nvim-tree/nvim-web-devicons' }
-    }
+	use({
+		"goolord/alpha-nvim",
+		requires = { "nvim-tree/nvim-web-devicons" },
+	})
 
-    use 'nvim-tree/nvim-web-devicons'
+	use("nvim-tree/nvim-web-devicons")
 
-    use { "kylechui/nvim-surround",
-        tag = "*",
-    }
+	use({ "kylechui/nvim-surround", tag = "*" })
 
-    use {
-        "folke/trouble.nvim",
-        requires = {
-            "nvim-tree/nvim-web-devicons"
-        }
-    }
+	use({
+		"folke/trouble.nvim",
+		requires = {
+			"nvim-tree/nvim-web-devicons",
+		},
+	})
 
-    use 'numToStr/Comment.nvim'
+	use("numToStr/Comment.nvim")
 
-    use "lewis6991/gitsigns.nvim"
+	use("lewis6991/gitsigns.nvim")
 
-    use "HiPhish/rainbow-delimiters.nvim"
+	use("HiPhish/rainbow-delimiters.nvim")
 
-    use 'andweeb/presence.nvim'
+	use("andweeb/presence.nvim")
 
-    use 'mhartington/formatter.nvim'
+	use("mhartington/formatter.nvim")
+
+	use({ "folke/todo-comments.nvim", requires = {
+		"nvim-lua/plenary.nvim",
+	} })
 end)
