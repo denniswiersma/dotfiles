5e9e803717d46fa3370b3d6a840adf20e88d76c7
denniswiersma <dennis.wiersma@icloud.com>
2023-11-06 11:03:44 +0100

    added git-worktree

---
+++ .config/nvim/after/plugin/git-worktree.lua
@@ -0,0 +1,14 @@
+require("telescope").load_extension("git_worktree")
+
+vim.keymap.set(
+	"n",
+	"<leader>sw",
+	"<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<cr>",
+	{ noremap = true, silent = true }
+)
+vim.keymap.set(
+	"n",
+	"<leader>nw",
+	"<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>",
+	{ noremap = true, silent = true }
+)
