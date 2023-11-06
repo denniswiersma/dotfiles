c1f525e6ab0a4169ff9dee7595fac6a25025a1bb
denniswiersma <dennis.wiersma@icloud.com>
2023-11-06 12:08:20 +0100

    removed silent and noremap

--- .config/nvim/after/plugin/git-worktree.lua
+++ .config/nvim/after/plugin/git-worktree.lua
@@ -3,12 +3,12 @@ require("telescope").load_extension("git_worktree")
 vim.keymap.set(
 	"n",
 	"<leader>sw",
-	"<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<cr>",
-	{ noremap = true, silent = true }
+	"<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<cr>"
+	-- { noremap = true, silent = true }
 )
 vim.keymap.set(
 	"n",
 	"<leader>nw",
-	"<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>",
-	{ noremap = true, silent = true }
+	"<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>"
+	-- { noremap = true, silent = true }
 )
