e2788c410c218a0070487d5518f9f9ee9c7f30f0
denniswiersma <dennis.wiersma@icloud.com>
2023-11-07 15:59:31 +0100

    removed redundent statements

--- .config/nvim/after/plugin/git-worktree.lua
+++ .config/nvim/after/plugin/git-worktree.lua
@@ -4,11 +4,9 @@ vim.keymap.set(
 	"n",
 	"<leader>sw",
 	"<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<cr>"
-	-- { noremap = true, silent = true }
 )
 vim.keymap.set(
 	"n",
 	"<leader>nw",
 	"<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>"
-	-- { noremap = true, silent = true }
 )
