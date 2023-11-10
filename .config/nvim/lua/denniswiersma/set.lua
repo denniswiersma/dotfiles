dedf4d24503cc0eff3f278c9683c04f993d9e8a7
denniswiersma <dennis.wiersma@icloud.com>
2023-11-10 16:25:14 +0100

    open neotree on startup instead of netrw

--- .config/nvim/lua/denniswiersma/set.lua
+++ .config/nvim/lua/denniswiersma/set.lua
@@ -40,3 +40,8 @@ for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	pattern = "*",
+	command = "Neotree position=current",
+})
