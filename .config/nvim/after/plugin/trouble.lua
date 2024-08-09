87edd060395c8cc6c7a015bd7b38304b06ea5d3e
Dennis Wiersma <hello@denniswiersma.com>
2024-08-09 08:57:30 +0200

    updated for trouble breaking changes

--- .config/nvim/after/plugin/trouble.lua
+++ .config/nvim/after/plugin/trouble.lua
@@ -1,4 +1,6 @@
 require("trouble").setup({
-    vim.keymap.set("n", "<leader>t", function() require("trouble").toggle() end),
-    use_diagnostic_signs = true
+	vim.keymap.set("n", "<leader>at", "<cmd> Trouble diagnostics toggle <cr>"),
+	vim.keymap.set("n", "<leader>t", "<cmd> Trouble diagnostics toggle filter.buf=0<cr>"),
+	use_diagnostic_signs = true,
+	focus = true,
 })
