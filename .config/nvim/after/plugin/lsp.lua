589181f527c0549106eb228595fa8f27d5b11d96
Dennis Wiersma <hello@denniswiersma.com>
2024-08-09 08:55:53 +0200

    added rust leptos setup

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -16,6 +16,22 @@ lsp.ensure_installed({
 	"rust_analyzer",
 })
 
+lsp.configure("rust_analyzer", {
+	settings = {
+		["rust-analyzer"] = {
+			procMacro = {
+				ignored = {
+					leptos_macro = {
+						-- optional: --
+						"component",
+						"server",
+					},
+				},
+			},
+		},
+	},
+})
+
 -- Fix Undefined global 'vim'
 lsp.nvim_workspace()
 
